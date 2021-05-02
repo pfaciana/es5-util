42aa39ebb9714ead3c3a3113aba6817200d6ec86
pfaciana <phil@renderdev.com>
2021-05-02 04:07:56 -0400

    fix whitespacing

--- gulpfile.js
+++ gulpfile.js
@@ -15,46 +15,43 @@ const options = {
 };
 
 const builds = {
-		js: {
+	js: {
+		configs: [{
+			id: 'es5-util',
+			src: './index.js',
+			dest: './dist',
+			watch: ['./index.js', './js/**/*.js'],
+			alsoMin: true,
+			browserify: true,
+			post: ['clean'],
+		}],
+		cb(_) {
+			return gulp.src(_.src)
+				.pipe(gulpif(_.browserify, browserifyBuffer()))
+				.pipe(rename({basename: 'es5-util'}))
+				.pipe(gulpif(_.minify, uglify()))
+				.pipe(gulpif(_.minify, rename({suffix: '.min'})))
+				.pipe(gulpif(_.debug, debug(_.debug)))
+				.pipe(gulpif(_.minify && _.size, size(_.size)))
+				.pipe(gulp.dest(_.dest))
+				.pipe(gulpif(_.minify, brotli.compress({extension: 'br', quality: 11})))
+				.pipe(gulpif(_.minify && _.sizeNgz, size(_.sizeNgz)))
+				.pipe(gulpif(_.minify, gulp.dest(_.dest)));
+		}
+	},
+	clean:
+		{
 			configs: [{
-				id: 'es5-util',
-				src: './index.js',
-				dest: './dist',
-				watch: ['./index.js', './js/**/*.js'],
-				alsoMin: true,
-				browserify: true,
-				post: ['clean'],
+				id: 'all',
+				debug: {title: 'clean:all'},
 			}],
 			cb(_) {
-				return gulp.src(_.src)
-					.pipe(gulpif(_.browserify, browserifyBuffer()))
-					.pipe(rename({basename: 'es5-util'}))
-					.pipe(gulpif(_.minify, uglify()))
-					.pipe(gulpif(_.minify, rename({suffix: '.min'})))
-					.pipe(gulpif(_.debug, debug(_.debug)))
-					.pipe(gulpif(_.minify && _.size, size(_.size)))
-					.pipe(gulp.dest(_.dest))
-					.pipe(gulpif(_.minify, brotli.compress({extension: 'br', quality: 11})))
-					.pipe(gulpif(_.minify && _.sizeNgz, size(_.sizeNgz)))
-					.pipe(gulpif(_.minify, gulp.dest(_.dest)));
-			}
+				return gulp.src(_.exclude, {base: (_.base || './'), since: gulp.lastRun(_.cb)})
+					.pipe(gulpif(_.ceol, ceol(_.ceol)))
+					.pipe(gulpif(buffer => !buffer.isDirectory(), gulpif(_.debug, debug(_.debug))))
+					.pipe(gulp.dest((_.dest || './')));
+			},
 		},
-		clean:
-			{
-				configs: [{
-					id: 'all',
-					debug: {title: 'clean:all'},
-				}],
-				cb(_) {
-					return gulp.src(_.exclude, {base: (_.base || './'), since: gulp.lastRun(_.cb)})
-						.pipe(gulpif(_.ceol, ceol(_.ceol)))
-						.pipe(gulpif(buffer => !buffer.isDirectory(), gulpif(_.debug, debug(_.debug))))
-						.pipe(gulp.dest((_.dest || './')));
-				}
-				,
-			}
-		,
-	}
-;
+};
 
 createTasks(builds, options);
